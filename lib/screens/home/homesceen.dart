222f794180a55eab8a6d2217e580e5b52c6d9cb7
Suhaibusman <suhaibusman54@gmail.com>
2023-08-31 15:20:35 -0700

    shopping app

--- lib/screens/home/homesceen.dart
+++ lib/screens/home/homesceen.dart
@@ -12,32 +12,126 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
-body: SafeArea(child: Container(
-  color: const Color(0xff2A4BA0),
-  width: double.infinity,
-  height: 252,
-  child:  Column(
-    children: [
-      Padding(
-        padding: const EdgeInsets.only(top: 40,left: 20,right: 20),
-        child: Row(
-         
-          children: [
-            const Text("Hey, Halal",style: TextStyle(color: Color(0xffF8F9FB
-      ),
-      fontSize: 22
-      ),),
-            SvgPicture.asset("assets/images/Cart Icon.svg")
-          ],
-        ),
-      )
-    ],
-  ),
-)),
-    
+      body: SafeArea(
+          child: Column(
+        children: [
+          Container(
+            color: const Color(0xff2A4BA0),
+            width: double.infinity,
+            height: 252,
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 40, left: 20, right: 20),
+                  child: Row(
+                    children: [
+                      const Expanded(
+                        child: Text(
+                          "Hey, Halal",
+                          style: TextStyle(
+                              color: Color(0xffF8F9FB),
+                              fontSize: 22,
+                              fontFamily: "Manrope",
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      SvgPicture.asset(
+                        "assets/images/Cart Icon.svg",
+                        height: 30,
+                      )
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 35, left: 20, right: 20),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: const Color(0xff153075),
+                      contentPadding: const EdgeInsets.all(15),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(28)),
+                      hintText: 'Search Product or Store',
+                      hintStyle: const TextStyle(
+                          color: Color(0xff8891A5), fontSize: 14),
+                      prefixIcon: Padding(
+                        padding: const EdgeInsets.all(12),
+                        child:
+                            SvgPicture.asset("assets/images/Search Icon.svg"),
+                      ),
+                    ),
+                  ),
+                ),
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 30, left: 20),
+                      child: Row(
+                        children: [
+                          Text(
+                            "DELIVERY TO",
+                            style: TextStyle(
+                                fontFamily: "Manrope",
+                                fontSize: 11,
+                                fontWeight: FontWeight.bold,
+                                color:
+                                    const Color(0xffF8F9FB).withOpacity(0.5)),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 250),
+                            child: Text(
+                              "WITHIN",
+                              style: TextStyle(
+                                  fontFamily: "Manrope",
+                                  fontSize: 11,
+                                  fontWeight: FontWeight.bold,
+                                  color:
+                                      const Color(0xffF8F9FB).withOpacity(0.5)),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                 Row(
+                        children: [
+                      const  SizedBox(
+                          width: 20,
+                         ),
+                           const Text("Green Way 3000, Sylhet" ,style: TextStyle(
+                              
+                              fontFamily: "Manrope",fontSize: 14,
+                            
+                              color: Color(0xffF8F9FB)),
+                              
+                              ),
+                               Padding(
+                            padding: const EdgeInsets.only(left: 5),
+                            child: SvgPicture.asset("assets/images/arrow Iocn.svg"),
+                          ),
+                              const SizedBox(width: 150,),
+                          
+                          const Text("1 hour" ,style: TextStyle(
+                          
+                          fontFamily: "Manrope",fontSize: 14,
+                        
+                          color: Color(0xffF8F9FB)),
+                          
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 5),
+                            child: SvgPicture.asset("assets/images/arrow Iocn.svg"),
+                          )
 
-      
+                            
+                        ],
+                      ),
+              ],
+            ),
+          ),
+        ],
+      )),
     );
   }
 }
