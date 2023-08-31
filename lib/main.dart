14d8357eed9e9b54be77d9ef6ee0a2c0d0c16f56
Suhaibusman <suhaibusman54@gmail.com>
2023-08-31 16:19:05 -0700

    shopping app

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
     debugShowCheckedModeBanner: false,
-       home:  HomeScreen(),
+       home: HomeScreen(),
      // home: MyHomePage(),
     );
   }
--- lib/screens/home/homesceen.dart
+++ lib/screens/home/homesceen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:shoppingapp/models/discount_part.dart';
 
+// ignore: must_be_immutable
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -9,67 +11,113 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+ List<Discountpart> discount=[];
+
+ void _getDiscount() {
+
+ setState(() {
+    discount = Discountpart.getDiscount();
+  });
+}
+
+ @override
+ void initState(){
+   super.initState();
+  _getDiscount();
+ }
+
   @override
   Widget build(BuildContext context) {
+    _getDiscount();
     return Scaffold(
       body: SafeArea(
           child: Column(
         children: [
-          Container(
-            color: const Color(0xff2A4BA0),
-            width: double.infinity,
-            height: 252,
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 40, left: 20, right: 20),
-                  child: Row(
-                    children: [
-                      const Expanded(
-                        child: Text(
-                          "Hey, Halal",
-                          style: TextStyle(
-                              color: Color(0xffF8F9FB),
-                              fontSize: 22,
-                              fontFamily: "Manrope",
-                              fontWeight: FontWeight.bold),
-                        ),
+          topbar(),
+          const SizedBox(height: 20,),
+         ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: 2,
+          itemBuilder: (context, index) {
+           return Container(
+            height: 123,
+            width: 269,
+           color: Colors.amber,
+           child: const Text("check"),
+           );
+         },)
+        ],
+      )),
+    );
+  }
+
+  Container topbar() {
+    return Container(
+          color: const Color(0xff2A4BA0),
+          width: double.infinity,
+          height: 252,
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 40, left: 20, right: 20),
+                child: Row(
+                  children: [
+                    const Expanded(
+                      child: Text(
+                        "Hey, Halal",
+                        style: TextStyle(
+                            color: Color(0xffF8F9FB),
+                            fontSize: 22,
+                            fontFamily: "Manrope",
+                            fontWeight: FontWeight.bold),
                       ),
-                      SvgPicture.asset(
-                        "assets/images/Cart Icon.svg",
-                        height: 30,
-                      )
-                    ],
-                  ),
+                    ),
+                    SvgPicture.asset(
+                      "assets/images/Cart Icon.svg",
+                      height: 30,
+                    )
+                  ],
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 35, left: 20, right: 20),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: const Color(0xff153075),
-                      contentPadding: const EdgeInsets.all(15),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(28)),
-                      hintText: 'Search Product or Store',
-                      hintStyle: const TextStyle(
-                          color: Color(0xff8891A5), fontSize: 14),
-                      prefixIcon: Padding(
-                        padding: const EdgeInsets.all(12),
-                        child:
-                            SvgPicture.asset("assets/images/Search Icon.svg"),
-                      ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 35, left: 20, right: 20),
+                child: TextField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: const Color(0xff153075),
+                    contentPadding: const EdgeInsets.all(15),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(28)),
+                    hintText: 'Search Product or Store',
+                    hintStyle: const TextStyle(
+                        color: Color(0xff8891A5), fontSize: 14),
+                    prefixIcon: Padding(
+                      padding: const EdgeInsets.all(12),
+                      child:
+                          SvgPicture.asset("assets/images/Search Icon.svg"),
                     ),
                   ),
                 ),
-                Row(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(top: 30, left: 20),
-                      child: Row(
-                        children: [
-                          Text(
-                            "DELIVERY TO",
+              ),
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 30, left: 20),
+                    child: Row(
+                      children: [
+                        Text(
+                          "DELIVERY TO",
+                          style: TextStyle(
+                              fontFamily: "Manrope",
+                              fontSize: 11,
+                              fontWeight: FontWeight.bold,
+                              color:
+                                  const Color(0xffF8F9FB).withOpacity(0.5)),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 250),
+                          child: Text(
+                            "WITHIN",
                             style: TextStyle(
                                 fontFamily: "Manrope",
                                 fontSize: 11,
@@ -77,61 +125,49 @@ class _HomeScreenState extends State<HomeScreen> {
                                 color:
                                     const Color(0xffF8F9FB).withOpacity(0.5)),
                           ),
-                          Padding(
-                            padding: const EdgeInsets.only(left: 250),
-                            child: Text(
-                              "WITHIN",
-                              style: TextStyle(
-                                  fontFamily: "Manrope",
-                                  fontSize: 11,
-                                  fontWeight: FontWeight.bold,
-                                  color:
-                                      const Color(0xffF8F9FB).withOpacity(0.5)),
-                            ),
-                          ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-                 Row(
-                        children: [
-                      const  SizedBox(
-                          width: 20,
-                         ),
-                           const Text("Green Way 3000, Sylhet" ,style: TextStyle(
-                              
-                              fontFamily: "Manrope",fontSize: 14,
+                  ),
+                ],
+              ),
+               Row(
+                      children: [
+                    const  SizedBox(
+                        width: 20,
+                       ),
+                         const Text("Green Way 3000, Sylhet" ,style: TextStyle(
                             
-                              color: Color(0xffF8F9FB)),
-                              
-                              ),
-                               Padding(
-                            padding: const EdgeInsets.only(left: 5),
-                            child: SvgPicture.asset("assets/images/arrow Iocn.svg"),
-                          ),
-                              const SizedBox(width: 150,),
+                            fontFamily: "Manrope",fontSize: 14,
                           
-                          const Text("1 hour" ,style: TextStyle(
-                          
-                          fontFamily: "Manrope",fontSize: 14,
+                            color: Color(0xffF8F9FB)),
+                            
+                            ),
+                             Padding(
+                          padding: const EdgeInsets.only(left: 5),
+                          child: SvgPicture.asset("assets/images/arrow Iocn.svg"),
+                        ),
+                            const SizedBox(width: 150,),
                         
-                          color: Color(0xffF8F9FB)),
-                          
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(left: 5),
-                            child: SvgPicture.asset("assets/images/arrow Iocn.svg"),
-                          )
+                        const Text("1 hour" ,style: TextStyle(
+                        
+                        fontFamily: "Manrope",fontSize: 14,
+                      
+                        color: Color(0xffF8F9FB)),
+                        
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 5),
+                          child: SvgPicture.asset("assets/images/arrow Iocn.svg"),
+                        )
 
-                            
-                        ],
-                      ),
-              ],
-            ),
+                          
+                      ],
+                    ),
+            ],
           ),
-        ],
-      )),
-    );
+        );
   }
 }
+
+
